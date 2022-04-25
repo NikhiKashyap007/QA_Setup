e2304bbd7ff832cbc870118401c13006e69671ad
NikhiKashyap007 <104198165+NikhiKashyap007@users.noreply.github.com>
2022-04-25 12:07:06 +0530

    Update V1.1.1__Create_database_objects.sql

--- Migration/V1.1.1__Create_database_objects.sql
+++ Migration/V1.1.1__Create_database_objects.sql
@@ -1,6 +1,3 @@
 create or replace database github_demo_db;
 use database github_demo_db;
-use role accountadmin;
-grant all on database github_demo_db to role sysadmin;
-use role sysadmin;
 create schema temp_Schema;
