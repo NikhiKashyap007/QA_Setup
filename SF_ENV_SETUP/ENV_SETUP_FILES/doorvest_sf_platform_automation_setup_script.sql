70064a21ada0ee6cb842ecaa5c89cbe065edc3db
NikhiKashyap007 <104198165+NikhiKashyap007@users.noreply.github.com>
2022-04-25 18:26:49 +0530

    Update doorvest_sf_platform_automation_setup_script.sql

--- SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_platform_automation_setup_script.sql
+++ SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_platform_automation_setup_script.sql
@@ -1,12 +1,12 @@
 //enable text log
-!spool C:/doorvest/mapping_data/log.txt;
+!spool SF_ENV_SETUP/ENV_SETUP_FILES/mapping_data/log.txt;
 
 //enable variable substitution
 !set variable_substitution=true;
 
 
 //sql environment setup file location
-!define env_setup_file = "C:/doorvest/doorvest_sf_env_setup.sql";
+!define env_setup_file = "SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_env_setup.sql";
 
 //setup environment by calling file
 !source &{env_setup_file};
