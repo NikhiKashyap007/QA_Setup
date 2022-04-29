f6d2cc65f5df8120e72681c91c3e5fbbfa930bba
NikhiKashyap007 <104198165+NikhiKashyap007@users.noreply.github.com>
2022-04-29 12:25:32 +0530

    Delete Dev_env_setup.sql

--- SF_ENV_SETUP/Dev_env_setup.sql
+++
@@ -1,7 +0,0 @@
-//temp
-
-!source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_platform_automation_setup_script.sql
-
-!source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_environment_setup_script.sql
-
-call AUTOMATION_FRAMEWORK_DB.AFW_SCMA.P_AFW_SETUP('{{ VAR_ENV }}');
