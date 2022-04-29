59937f947c06a4283e405840e3326d49e935b276
NikhiKashyap007 <104198165+NikhiKashyap007@users.noreply.github.com>
2022-04-29 18:24:25 +0530

    Update Dev_env_setup.sql

--- SF_ENV_SETUP/Dev_env_setup.sql
+++ SF_ENV_SETUP/Dev_env_setup.sql
@@ -3,5 +3,5 @@
 !source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_platform_automation_setup_script.sql
 
 !source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_environment_setup_script.sql
-use role sysadmin;
+
 call AUTOMATION_FRAMEWORK_DB.AFW_SCMA.P_AFW_SETUP('DEV');
