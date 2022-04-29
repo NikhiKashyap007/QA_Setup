c813881dfd758db277c1f8e31524c98fc3b2393d
NikhiKashyap007 <104198165+NikhiKashyap007@users.noreply.github.com>
2022-04-29 17:43:00 +0530

    Update Dev_env_setup.sql

--- SF_ENV_SETUP/Dev_env_setup.sql
+++ SF_ENV_SETUP/Dev_env_setup.sql
@@ -4,4 +4,4 @@
 
 !source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_environment_setup_script.sql
 
-call AUTOMATION_FRAMEWORK_DB.AFW_SCMA.P_AFW_SETUP('QA');
+call AUTOMATION_FRAMEWORK_DB.AFW_SCMA.P_AFW_SETUP('DEV');
