3184bfd10691cf10a9d0f3843bbd8afb25b63565
NikhiKashyap007 <104198165+NikhiKashyap007@users.noreply.github.com>
2022-04-29 12:25:21 +0530

    Create ENV_setup_script.sql

---
+++ SF_ENV_SETUP/SCRIPT_FOR_ENV_SETUP/ENV_setup_script.sql
@@ -0,0 +1,7 @@
+//temp
+
+!source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_platform_automation_setup_script.sql
+
+!source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_environment_setup_script.sql
+
+call AUTOMATION_FRAMEWORK_DB.AFW_SCMA.P_AFW_SETUP('{{ VAR_ENV }}');
