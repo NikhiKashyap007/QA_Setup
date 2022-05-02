2bc7bd25c85cd9d95c11e84be187e44f3a9b1802
NikhiKashyap007 <104198165+NikhiKashyap007@users.noreply.github.com>
2022-05-02 15:48:50 +0530

    Update Env_setup_script.sql

--- SF_ENV_SETUP/ENV_SETUP_SCRIPTS/Env_setup_script.sql
+++ SF_ENV_SETUP/ENV_SETUP_SCRIPTS/Env_setup_script.sql
@@ -2,3 +2,5 @@
 !source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_platform_automation_setup_script.sql
 
 !source SF_ENV_SETUP/ENV_SETUP_FILES/doorvest_sf_environment_setup_script.sql
+
+use role sysadmin;
