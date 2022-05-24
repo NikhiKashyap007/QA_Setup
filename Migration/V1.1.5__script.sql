a83a3444a0a18bb8dfa797a3ca5335ab305343ce
NikhiKashyap007 <nikhikashyap@yahoo.com>
2022-05-24 17:42:10 +0530

    testing sonarscan

--- Migration/V1.1.5__script.sql
+++ Migration/V1.1.5__script.sql
@@ -1,7 +1,3 @@
 use database {{ VAR_LOAD_DB }};
 create schema temp_schema;
 create schema if not exists temp_schema;
-
-create schema if not exists temp_schema;
-
-create schema if not exists temp_schema;
